b74032b2fc32f87e7c7209f70fdbb415dccb0ac2
Rúben Silva <rubensilva945@gmail.com>
2018-01-05 14:36:49 +0000

    Atualizado metódo de gerar passwords, para um mais eficaz

--- passGenerate.py
+++ passGenerate.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 # Gerador de Passwords Seguras
-# Data: 17/07/2017
+# Data Inicio Projeto: 17/07/2017
+# Data Última Correção: 05/01/2018
 # Desenvolvedor: Rúben Silva
 
-# Imports
+# Imports necessários ao projeto
 
 import sys
 import random
@@ -12,16 +14,16 @@ from random import randint
 
 ###########
 
-# Classe Numeros
+# Classe responsável por gerar números aleatórios
 def numeros():
 	# Array com todos os numeros básicos
-	numeros = ['1', '2',  '3', '4', '5', '6', '7', '8', '9',]
+	numeros = ['1', '2',  '3', '4', '5', '6', '7', '8', '9', '0']
 	# Obtém números aleatórios
 	randomNumero = random.choice(numeros)
 	# Retorna esses numeros
 	return randomNumero
 
-# Classe Letras
+# Classe responsável por gerar letras aleatórias
 def letras():
 	# Array com letras comuns a vários teclados
 	letras = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
@@ -30,51 +32,60 @@ def letras():
 	# Retorna essas letras
 	return randomLetra
 
-# Classe Símbolos
+# Classe responsável por gerar símbolos aleatórios (os mais usais)
 def simbolos():
 	# Array com vários simbolos presentes no teclado
-	simbolos = ['|', '!', '#', '%', '&', '/', '(', ')', '=', '?', '«', '»', '@', '§', '{', '[', ']', '}', '<', '>', ',', '.', ';', ':', '-', '_', '^', '~', '+', '*', '¨']
+	simbolos = ['!', '#', '%', '&', '/', '(', ')', '=', '?', '«', '»', '@', '{', '[', ']', '}', '<', '>', ':', '-', '_', '+', '*']
 	# Obtém simbolos aleatórios
 	randomSimbolo = random.choice(simbolos)
 	# Retorna esses simbolos
 	return randomSimbolo
 
-# Classe Password
-def password(randomSimbolo0, randomLetra0, randomNumero0, randomSimbolo1, randomLetra1, randomNumero1):
-	pass1 = randomSimbolo0 + randomNumero0 + randomLetra0 + randomNumero1 + randomLetra1 + randomSimbolo1 # Estrutura Password
-	pass2 = pass1 # Pass 2 igual a pass1 (2 vezes a mesma para que não seja facil de pensa numa repetição)
+# Classe que gera a password
+def password(randomLetra0, randomNumero0, randomLetra1, randomLetra2, randomNumero1, randomSimbolo, randomNumero2, randomLetra3, randomNumero3, randomLetra4):
+	# pass1 = primeira estrutura da password
+	pass1 = randomLetra0 + randomNumero0 + randomLetra1 + randomLetra2 + randomNumero1
+	# pass2 = segunda estrutura da password
+	pass2 = randomSimbolo + randomNumero2 + randomLetra3 + randomNumero3 + randomLetra4
+	# Uniao de ambas as estruturas
 	password1 = pass1 + pass2
-	return password1 # Retorna a Password
+	# Retorna a Password
+	return password1
 
-# Classe ajuda
+# Classe que mostra a mensagem de ajuda
 def ajuda():
 	return """
 	COMO USAR: python passGenerate.py
 	"""
-###########
 
 # Verifica se o argumento 1 é o pedido de ajuda, e em caso afirmativo retorna a mensagem de ajuda
 if len(sys.argv) > 1:
 	if sys.argv[1] == '-h':
 		print (ajuda())
 		sys.exit(0)
-##########
-# Mensagem de Entrada
+
+# Mensagem de Entrada a cada execução
 print ("""
 +-----------------------------------------------------+
 |                    Pass Generate                    |
 +-----------------------------------------------------+
-                      Rúben Silva 
+                      Rúben Silva
 """)
 
 # Declaração das respetivas variáveis
-randomSimbolo0 = numeros()
+# Cada variavel, aceda a uma respetiva classe, onde é gerado o respetivo pedido
 randomLetra0 = letras()
-randomNumero0 = simbolos()
-randomSimbolo1 = numeros()
+randomNumero0 = numeros()
 randomLetra1 = letras()
-randomNumero1 = simbolos()
+randomLetra2 = letras()
+randomNumero1 = numeros()
+randomSimbolo = simbolos()
+randomNumero2 = numeros()
+randomLetra3 = letras()
+randomNumero3 = numeros()
+randomLetra4 = letras()
 
-password1 = password(randomSimbolo0, randomLetra0, randomNumero0, randomLetra1, randomSimbolo1, randomNumero1)
+# Processo que acessa a classe password para gerar uma password
+password1 = password(randomLetra0, randomNumero0, randomLetra1, randomLetra2, randomNumero1, randomSimbolo, randomNumero2, randomLetra3, randomNumero3, randomLetra4)
 
-print ("A sua Password: " + password1) 
+print ("A sua Password: " + password1)
